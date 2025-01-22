1bd870a1ddb9e118f393aec8dc7e49e33035191e
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-22 13:03:23 +0000

    feat (zl4323, bc2423) - implemented an Expr trait with inheriting case classes representing binary/unary operators and atoms

---
+++ src/main/wacc/expr.scala
@@ -0,0 +1,32 @@
+sealed trait Expr 
+
+// Binary operators
+case class Mul(x: Expr, y: Expr) extends Expr
+case class Div(x: Expr, y: Expr) extends Expr
+case class Mod(x: Expr, y: Expr) extends Expr
+case class Add(x: Expr, y: Expr) extends Expr
+case class Sub(x: Expr, y: Expr) extends Expr
+case class GreaterThan(x: Expr, y: Expr) extends Expr
+case class GreaterThanEq(x: Expr, y: Expr) extends Expr
+case class LessThan(x: Expr, y: Expr) extends Expr
+case class LessThanEq(x: Expr, y: Expr) extends Expr
+case class Eq(x: Expr, y: Expr) extends Expr
+case class NotEq(x: Expr, y: Expr) extends Expr
+case class And(x: Expr, y: Expr) extends Expr
+case class Or(x: Expr, y: Expr) extends Expr
+
+// Unary operators
+case class Not(x: Expr) extends Expr
+case class Neg(x: Expr) extends Expr
+case class Len(x: Expr) extends Expr
+case class Ord(x: Expr) extends Expr
+case class Chr(x: Expr) extends Expr
+
+// Atoms
+case class IntLiteral(v: Int) extends Expr
+case class BoolLiteral(v: Bool) extends Expr
+case class CharLiteral(v: Char) extends Expr
+case class StringLiteral(v: String) extends Expr
+case class PairNullLiteral extends Expr
+case class Ident(v: String) extends Expr
+case class ArrayElem(v: Ident, is: List[Expr]) extends Expr
