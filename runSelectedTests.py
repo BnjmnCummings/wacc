a191bf48dfe29f9f83a5dc2bc2fa815ee17bd6e2
Aidan <aidanmadge@gmail.com>
2025-01-23 02:31:52 +0000

    ci - simplified complex code no longer needed after naming scheme change

--- runSelectedTests.py
+++ runSelectedTests.py
@@ -23,18 +23,12 @@ def extract_tests(data):
     tests = []
     for key, value in data.items():
         if isinstance(value, list):
-            for item in value:
-                tests.append(f"{key}_{item}")
-        elif isinstance(value, dict):
-            nested_tests = extract_tests(value)
-            for nested_test in nested_tests:
-                tests.append(f"{key}_{nested_test}")
+            for name in value:
+                tests.append(f"{key}_{name}")
     return tests
 
 def run_test(test_name):
     command = f'scala test . --test-only "*{test_name}*"'
-    p = subprocess.run(command, shell=True)
-    print(p.stdout)
-    return p
+    return subprocess.run(command, shell=True)
 
 main()
