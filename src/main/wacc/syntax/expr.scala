ba6abb53976d932c908c7023fcb85255fed2160d
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-27 17:58:58 +0000

    feat - wrote parser bridges for the AST nodes in expr.scala

--- src/main/wacc/syntax/expr.scala
+++ src/main/wacc/syntax/expr.scala
@@ -1,5 +1,7 @@
 package wacc.syntax
 
+import parsley.generic
+
 sealed trait Expr extends RValue
 sealed trait LValue
 sealed trait RValue
@@ -20,6 +22,21 @@ case class NotEq(x: Expr, y: Expr) extends BinaryOper
 case class And(x: Expr, y: Expr) extends BinaryOper
 case class Or(x: Expr, y: Expr) extends BinaryOper
 
+object Mul extends generic.ParserBridge2[Expr, Expr, Mul]
+object Div extends generic.ParserBridge2[Expr, Expr, Div]
+object Mod extends generic.ParserBridge2[Expr, Expr, Mod]
+object Add extends generic.ParserBridge2[Expr, Expr, Add]
+object Sub extends generic.ParserBridge2[Expr, Expr, Sub]
+object GreaterThan extends generic.ParserBridge2[Expr, Expr, GreaterThan]
+object GreaterThanEq extends generic.ParserBridge2[Expr, Expr, GreaterThanEq]
+object LessThan extends generic.ParserBridge2[Expr, Expr, LessThan]
+object LessThanEq extends generic.ParserBridge2[Expr, Expr, LessThanEq]
+object Eq extends generic.ParserBridge2[Expr, Expr, Eq]
+object NotEq extends generic.ParserBridge2[Expr, Expr, NotEq]
+object And extends generic.ParserBridge2[Expr, Expr, And]
+object Or extends generic.ParserBridge2[Expr, Expr, Or]
+
+
 // Unary operators
 sealed trait UnaryOper extends Expr
 case class Not(x: Expr) extends UnaryOper
@@ -27,6 +44,13 @@ case class Neg(x: Expr) extends UnaryOper
 case class Len(x: Expr) extends UnaryOper
 case class Ord(x: Expr) extends UnaryOper
 case class Chr(x: Expr) extends UnaryOper
+
+object Not extends generic.ParserBridge1[Expr, Not]
+object Neg extends generic.ParserBridge1[Expr, Neg]
+object Len extends generic.ParserBridge1[Expr, Len]
+object Ord extends generic.ParserBridge1[Expr, Ord]
+object Chr extends generic.ParserBridge1[Expr, Chr]
+
 
 // Atoms
 case class IntLiteral(v: BigInt) extends Expr
@@ -40,6 +64,15 @@ case class ArrayElem(v: String, indicies: List[Expr]) extends Expr, LValue
 
 object PairNullLiteral extends Expr
 
+object IntLiteral extends generic.ParserBridge1[BigInt, IntLiteral]
+object BoolLiteral extends generic.ParserBridge1[Boolean, BoolLiteral]
+object EscapedCharLiteral extends generic.ParserBridge1[Char, EscapedCharLiteral]
+object StandardCharLiteral extends generic.ParserBridge1[Char, StandardCharLiteral]
+object StringLiteral extends generic.ParserBridge1[List[CharLiteral], StringLiteral]
+object Ident extends generic.ParserBridge1[String, Ident]
+object ArrayElem extends generic.ParserBridge2[String, List[Expr], ArrayElem]
+
+
 // RValues
 case class FuncCall(v: String, args: List[Expr]) extends RValue
 case class ArrayLiteral(xs: List[Expr]) extends RValue
@@ -50,3 +83,8 @@ enum PairIndex {
   case First
   case Second
 }
+
+object FuncCall extends generic.ParserBridge2[String, List[Expr], FuncCall]
+object ArrayLiteral extends generic.ParserBridge1[List[Expr], ArrayLiteral]
+object PairElem extends generic.ParserBridge2[PairIndex, LValue, PairElem]
+object NewPair extends generic.ParserBridge2[Expr, Expr, NewPair]
