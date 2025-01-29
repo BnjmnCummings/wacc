727d41559b8c54967c4c18092915cfd2664c56d6
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-29 20:32:48 +0000

    refactor - pulled out data for the textDesc into a separate file to reduce clutter in lexer.scala (and fixed bug in the bool parser preventing testing)

---
+++ src/main/wacc/lexer-desc-data.scala
@@ -0,0 +1,22 @@
+package wacc
+
+val escapedMapping = Map(
+                    "0" -> 0x00, 
+                    "b" -> 0x08,
+                    "t" -> 0x09,
+                    "n" -> 0x0a,
+                    "f" -> 0x0c,
+                    "r" -> 0x0d,
+                )
+
+val hardKwdsSet = Set("null", "if", "then", "else", "fi", "while", 
+                "do", "done", "newpair", "fst", "snd", 
+                "read", "free", "return", "exit", "print", "call",
+                "println", "int", "bool", "char", "string", "pair", 
+                "begin", "end", "skip", "len", "ord", "chr")
+
+val escapedLiterals = Set('\"','\'','\\')
+
+val commentStart = "#"
+
+val sEnds = Set(("\"", "\""))
