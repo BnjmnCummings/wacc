fa08ed428a6e9b78a22941e560ccf69b3dab92df
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-29 17:09:18 +0000

    feat (zl4323, bc2423) - filled out the lexicalDesc object in lexer.scala to specify comments/escaped characters/etc

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -1,8 +1,13 @@
 package wacc
 
 import parsley.Parsley
-import parsley.token.{Lexer, Basic}
+import parsley.token.{Lexer, Basic, Unicode}
 import parsley.token.descriptions.*
+
+import scala.collection.immutable.NumericRange
+
+val FIRST_GRAPHIC_ASCII = '!'
+val LAST_GRAPHIC_ASCII = '~'
 
 object lexer {
     private val desc = LexicalDesc.plain.copy(
@@ -10,21 +15,41 @@ object lexer {
             identifierStart = Basic(_.isLetter),
             identifierLetter = Basic(_.isLetterOrDigit),
         ),
-        spaceDesc = SpaceDesc.plain,
-        symbolDesc = SymbolDesc.plain.copy(
-            hardKeywords = HARD_KEYWORDS,
+        spaceDesc = SpaceDesc.plain.copy(
+            lineCommentAllowsEOF = true,
+            lineCommentStart = "#",
         ),
+        symbolDesc = SymbolDesc.plain.copy(
+            hardKeywords = Set("null", "if", "then", "else", "fi", "while", 
+                "do", "done", "for", "newpair", "fst", "snd", 
+                "read", "free", "return", "exit", "print", "call",
+                "println", "int", "bool", "char", "string", "pair", 
+                "begin", "end", "skip", "len", "ord", "chr"),
+            caseSensitive = true,
+        ),
+        textDesc = TextDesc.plain.copy(
+            characterLiteralEnd = '\'',
+            stringEnds = Set(("\"", "\"")),
+            escapeSequences = EscapeDesc.plain.copy(
+                escBegin = '\\',
+                literals = Set('\"','\'','\\'),
+                mapping = Map(
+                    "0" -> 0x00, 
+                    "b" -> 0x08,
+                    "t" -> 0x09,
+                    "n" -> 0x0a,
+                    "f" -> 0x0c,
+                    "r" -> 0x0d,
+                ),
+            ),
+            graphicCharacter = 
+                // Not sure if \ " and ' should be included or excluded here
+                Basic(NumericRange.inclusive[Char](FIRST_GRAPHIC_ASCII,LAST_GRAPHIC_ASCII,1)),
+        )
     )
     private val lexer = Lexer(desc)
 
-    private val HARD_KEYWORDS = 
-        Set("null", "if", "then", "else", "fi", "while", 
-            "do", "done", "for", "newpair", "fst", "snd", 
-            "read", "free", "return", "exit", "print", "call",
-            "println", "int", "bool", "char", "string", "pair", 
-            "begin", "end", "skip", "len", "ord", "chr")
-
-    val _int: Parsley[BigInt] = lexer.lexeme.integer.decimal
+    val _int: Parsley[BigInt] = lexer.lexeme.integer.decimal32
     val _ident: Parsley[String] = lexer.lexeme.names.identifier
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
