f8fa08ab5a467fee8649b8237a8473b08aa4fcf3
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-29 15:56:07 +0000

    fix - corrected hard keywords in the lexer

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -12,10 +12,17 @@ object lexer {
         ),
         spaceDesc = SpaceDesc.plain,
         symbolDesc = SymbolDesc.plain.copy(
-            hardKeywords = Set("null", "if", "then", "else", "fi", "while", "do", "done", "for", "newpair", "fst", "snd", "read", "free", "return", "exit", "print", "println", "int", "bool", "char", "string", "pair", "array", "begin", "end", "isnull", "skip", "len", "ord", "chr"),
+            hardKeywords = HARD_KEYWORDS,
         ),
     )
     private val lexer = Lexer(desc)
+
+    private val HARD_KEYWORDS = 
+        Set("null", "if", "then", "else", "fi", "while", 
+            "do", "done", "for", "newpair", "fst", "snd", 
+            "read", "free", "return", "exit", "print", "call",
+            "println", "int", "bool", "char", "string", "pair", 
+            "begin", "end", "skip", "len", "ord", "chr")
 
     val _int: Parsley[BigInt] = lexer.lexeme.integer.decimal
     val _ident: Parsley[String] = lexer.lexeme.names.identifier
