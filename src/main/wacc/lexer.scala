cf57a3cc52f936d7773df54e563b40fa79a7977a
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-29 20:19:28 +0000

    revert - "refactor - pulled out data for the textDesc into a separate file to reduce clutter in lexer.scala"
    This reverts commit 078b6c5095f5cc530b834a1f8758ec1f54320721.

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -4,14 +4,11 @@ import parsley.Parsley
 import parsley.token.{Lexer, Basic}
 import parsley.token.descriptions.*
 
-import lexer.implicits.implicitSymbol
-
 object lexer {
-
     private val desc = LexicalDesc.plain.copy(
         nameDesc = NameDesc.plain.copy(
-            identifierStart = Basic(idStart),
-            identifierLetter = Basic(idEnd),
+            identifierStart = Basic(_.isLetter),
+            identifierLetter = Basic(_.isLetterOrDigit),
         ),
         spaceDesc = SpaceDesc.plain.copy(
             lineCommentAllowsEOF = true,
@@ -19,7 +16,7 @@ object lexer {
         ),
         symbolDesc = SymbolDesc.plain.copy(
             hardKeywords = Set("null", "if", "then", "else", "fi", "while", 
-                "do", "done", "newpair", "fst", "snd", 
+                "do", "done", "for", "newpair", "fst", "snd", 
                 "read", "free", "return", "exit", "print", "call",
                 "println", "int", "bool", "char", "string", "pair", 
                 "begin", "end", "skip", "len", "ord", "chr"),
@@ -31,22 +28,24 @@ object lexer {
             escapeSequences = EscapeDesc.plain.copy(
                 escBegin = '\\',
                 literals = Set('\"','\'','\\'),
-                mapping = escapedMapping
+                mapping = Map(
+                    "0" -> 0x00, 
+                    "b" -> 0x08,
+                    "t" -> 0x09,
+                    "n" -> 0x0a,
+                    "f" -> 0x0c,
+                    "r" -> 0x0d,
+                ),
             ),
             graphicCharacter = Basic(c => !(Set('\"', '\'', '\\').contains(c))),
         )
     )
-
     private val lexer = Lexer(desc)
 
     val _int: Parsley[BigInt] = lexer.lexeme.integer.decimal32
     val _ident: Parsley[String] = lexer.lexeme.names.identifier
     val _char: Parsley[Char] = lexer.lexeme.character.ascii
     val _string: Parsley[String] = lexer.lexeme.string.ascii
-    val _bool: Parsley[Boolean] = lexer.lexeme(("true" as true) | ("false" as false))
     val implicits = lexer.lexeme.symbol.implicits
     def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
-
-    def idStart(c: Char): Boolean = c.isLetter
-    def idEnd(c: Char): Boolean = c.isLetterOrDigit
 }
