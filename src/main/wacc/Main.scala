3a69a9f1ef1d403479c8d71a7eff65b7970433cc
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-31 17:02:42 +0000

    feat - wrote main function to read a file and parse its contents in Main.scala

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -2,15 +2,27 @@ package wacc
 
 import parsley.{Success, Failure}
 
+import os._
+
 import scala.util.Random
 
 def main(args: Array[String]): Unit = {
-    carrot(args)
     args.headOption match {
         // change parse to parseFile if you can figure out how to use it
-        case Some(fname) => parser.parse(fname) match
-            case Success(x) => sys.exit(0)
-            case Failure(msg) => sys.exit(0)
+        case Some(_) => {
+            val fname = "arrayBasic.wacc"
+            val fileContents: String = os.read(os.pwd / fname)
+            parser.parse(fileContents) match
+                case Success(x) => {
+                    println(x)
+                    // check semantics here 
+                    sys.exit(0)
+                }
+                case Failure(msg) => {
+                    println(msg)
+                    sys.exit(100)
+                }
+        }
         case None => println("please enter an expression")
     }
 }
