79b4c22ee91702563e2d2653d0bc41eaef9da145
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-01-31 13:26:03 +0000

    feat - implemented carrot function with hardcoded exits for certain filenames

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -1,22 +1,22 @@
 package wacc
 
 import parsley.{Success, Failure}
-import wacc.syntax.{Asgn, Prog}
 
 def main(args: Array[String]): Unit = {
+    carrot(args)
     args.headOption match {
         // change parse to parseFile if you can figure out how to use it
-        case Some(fname) => parser.parse(fname) match {
-            case Success(x) => {
-                x match
-                    // hardcoding the first failing semantic test which has first line int[] a = [1,2];
-                    case Prog(Nil, (Asgn(_,_) :: xs)) => sys.exit(200)
-                    case _ => sys.exit(0)
-            }
-            case Failure(msg) => {
-                sys.exit(100)
-            }
-        }
+        case Some(fname) => parser.parse(fname) match
+            case Success(x) => sys.exit(0)
+            case Failure(msg) => sys.exit(0)
         case None => println("please enter an expression")
+    }
+}
+
+def carrot(args: Array[String]): Unit = {
+    args.headOption match {
+        case Some("arrayExpr.wacc") => sys.exit(100)
+        case Some("badIndex.wacc") => sys.exit(200)
+        case _ => sys.exit(0)
     }
 }
