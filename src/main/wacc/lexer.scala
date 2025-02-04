99b6358d756de922fc2f0d11621abcd05eb6b579
zakk <zakk.lazar23@imperial.ac.uk>
2025-02-04 16:08:40 +0000

    feat - filled out a labelSymbol function in the lexer and added function stubs of other potentially useful functions

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -4,6 +4,7 @@ import parsley.Parsley
 import parsley.Parsley.atomic
 import parsley.token.{Lexer, Basic}
 import parsley.token.descriptions.*
+import parsley.token.errors.*
 
 import parsley.character.{string}
 // import lexer.implicits.implicitSymbol
@@ -34,7 +35,62 @@ object lexer {
         )
     )
 
-    private val lexer = Lexer(desc)
+    private val errConfig = new ErrorConfig {
+        // name errors
+        override def filterNameIllFormedIdentifier: FilterConfig[String] = ???
+        override def labelNameIdentifier: String = ???
+        override def unexpectedNameIllegalIdentifier(v: String): String = ???
+
+        // numeric errors
+        override def filterIntegerOutOfBounds(min: BigInt, max: BigInt, nativeRadix: Int): FilterConfig[BigInt] = ???
+        override def labelIntegerSignedNumber: LabelWithExplainConfig = ???
+        override def labelIntegerUnsignedNumber: LabelWithExplainConfig = ???
+
+        // space errors
+        override def labelSpaceEndOfLineComment: LabelWithExplainConfig = ???
+
+        // symbol errors
+        override def labelSymbol: Map[String, LabelWithExplainConfig] = {
+            Map(
+                "+" -> Label("arithmetic operator"), 
+                "*" -> Label("arithmetic operator"),
+                "-" -> Label("arithmetic operator"),
+                "/" -> Label("arithmetic operator"),
+                "%" -> Label("arithmetic operator"),
+                ">" -> Label("comparison operator"),
+                ">=" -> Label("comparison operator"),
+                "<" -> Label("comparison operator"),
+                "<=" -> Label("comparison operator"),
+                "==" -> Label("comparison operator"), 
+                "!=" -> Label("comparison operator"), 
+                "&&" -> Label("boolean operator"), 
+                "||" -> Label("boolean operator"),
+                "!" -> Label("prefix operator"),
+                "len" -> Label("prefix operator"),
+                "ord" -> Label("prefix operator"),
+                "chr" -> Label("prefix operator"),
+                "(" -> Label("bracketed expression"),
+                ")" -> LabelAndReason("unclosed brackets", "closing bracket"),
+            )
+        }
+
+        override def labelSymbolEndOfKeyword(symbol: String): String = ???
+
+        // text errors
+        override def filterCharNonAscii: VanillaFilterConfig[Int] = ???
+        override def filterStringNonAscii: SpecializedFilterConfig[StringBuilder] = ???
+        override def labelCharAscii: LabelWithExplainConfig = ???
+        override def labelCharAsciiEnd: LabelConfig = ???
+        override def labelEscapeSequence: LabelWithExplainConfig = ???
+        override def labelGraphicCharacter: LabelWithExplainConfig = ???
+        override def labelStringAscii(multi: Boolean, raw: Boolean): LabelWithExplainConfig = ???
+        override def labelStringAsciiEnd(multi: Boolean, raw: Boolean): LabelConfig = ???
+        override def verifiedCharBadCharsUsedInLiteral: VerifiedBadChars = ???
+        override def verifiedStringBadCharsUsedInLiteral: VerifiedBadChars = ???
+
+    }
+
+    private val lexer = Lexer(desc, errConfig)
 
     val _int: Parsley[BigInt] = lexer.lexeme.integer.decimal32
     val _ident: Parsley[String] = lexer.lexeme.names.identifier
