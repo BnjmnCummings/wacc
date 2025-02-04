507d999ed70fcb48e51725b208d6065164b3b44d
Aidan <aidanmadge@gmail.com>
2025-02-04 22:10:07 +0000

    add - name generation and management

--- src/main/wacc/renamer.scala
+++ src/main/wacc/renamer.scala
@@ -14,6 +14,7 @@ object renamer {
     private def rename(funcs: List[Func]): List[Q_Func] = ???
     
     private def rename(func: Func): Q_Func = ???
+    
     private def rename(param: Param): Q_Param = ???
     
     private def rename(stmts: List[Stmt], parScope: collection.immutable.Set[Q_Name], localScope: collection.immutable.Set[Q_Name]): List[Q_Stmt] = ???
@@ -25,4 +26,20 @@ object renamer {
     private def rename(rvalue: RValue, scope: collection.immutable.Set[Q_Name]): Q_RValue = ???
 
     private def rename(expr: Expr, scope: collection.immutable.Set[Q_Name]): Q_Expr = ???
+    
+    
+    private def genName(name: String): Q_Name = {
+        val count = name_gen_table.getOrElse(name, 0)
+        name_gen_table(name) = count + 1
+        Q_Name(name, s"${name}/$count")
+    }
+
+    private def updateName(name: String, localScope: collection.immutable.Set[Q_Name]): Q_Name = {
+        if localScope.exists(_.old_name == name) then
+            localScope.find(_.old_name == name).get
+        else if globalScope.exists(_.old_name == name) then
+            globalScope.find(_.old_name == name).get 
+        else
+            genName(name)
+    }
 }
