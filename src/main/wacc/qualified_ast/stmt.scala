8df89281fcf3fa2023811c2585c0e2fe6173c1fd
Aidan <aidanmadge@gmail.com>
2025-02-04 22:53:10 +0000

    feat - added scoped attributes for scoped variables for scoping statmenets

--- src/main/wacc/qualified_ast/stmt.scala
+++ src/main/wacc/qualified_ast/stmt.scala
@@ -13,9 +13,9 @@ case class Q_Return(x: Q_Expr) extends Q_Stmt
 case class Q_Exit(x: Q_Expr) extends Q_Stmt
 case class Q_Print(x: Q_Expr) extends Q_Stmt
 case class Q_Println(x: Q_Expr) extends Q_Stmt
-case class Q_If(cond: Q_Expr, body: List[Q_Stmt], el: List[Q_Stmt]) extends Q_Stmt
-case class Q_While(cond: Q_Expr, body: List[Q_Stmt]) extends Q_Stmt
-case class Q_CodeBlock(body: List[Q_Stmt]) extends Q_Stmt
+case class Q_If(cond: Q_Expr, body: List[Q_Stmt], scoped_body: List[Q_Name], el: List[Q_Stmt], scoped_el: List[Q_Name]) extends Q_Stmt
+case class Q_While(cond: Q_Expr, body: List[Q_Stmt], scoped: List[Q_Name]) extends Q_Stmt
+case class Q_CodeBlock(body: List[Q_Stmt], scoped: List[Q_Name]) extends Q_Stmt
 
 object Q_Decl extends generic.ParserBridge3[Type, Q_Name, Q_RValue, Q_Decl]
 object Q_Asgn extends generic.ParserBridge2[Q_LValue, Q_RValue, Q_Asgn]
@@ -25,8 +25,8 @@ object Q_Return extends generic.ParserBridge1[Q_Expr, Q_Return]
 object Q_Exit extends generic.ParserBridge1[Q_Expr, Q_Exit]
 object Q_Print extends generic.ParserBridge1[Q_Expr, Q_Print]
 object Q_Println extends generic.ParserBridge1[Q_Expr, Q_Println]
-object Q_If extends generic.ParserBridge3[Q_Expr, List[Q_Stmt], List[Q_Stmt], Q_If]
-object Q_While extends generic.ParserBridge2[Q_Expr, List[Q_Stmt], Q_While]
-object Q_CodeBlock extends generic.ParserBridge1[List[Q_Stmt], Q_CodeBlock]
+object Q_If extends generic.ParserBridge5[Q_Expr, List[Q_Stmt], List[Q_Name], List[Q_Stmt], List[Q_Name], Q_If]
+object Q_While extends generic.ParserBridge3[Q_Expr, List[Q_Stmt], List[Q_Name], Q_While]
+object Q_CodeBlock extends generic.ParserBridge2[List[Q_Stmt], List[Q_Name], Q_CodeBlock]
 
 object Q_Skip extends Q_Stmt
