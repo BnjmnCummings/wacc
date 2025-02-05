99ae37a90dc20e08d1a0bdf9f52ecc92876a6144
BnjmnCummings <cummingsben097@gmail.com>
2025-02-05 14:29:32 +0000

    feat - added test skeleton for renaming

---
+++ src/test/wacc/semantic/rename-expr.test.scala
@@ -0,0 +1,10 @@
+package test.wacc.semantic
+
+import org.scalatest.funsuite.AnyFunSuite
+import wacc.ast.*
+import wacc.q_ast.*
+
+class rename_expr_test extends AnyFunSuite {
+
+
+}
---
+++ src/test/wacc/semantic/rename-func.test.scala
@@ -0,0 +1,10 @@
+package test.wacc.semantic
+
+import org.scalatest.funsuite.AnyFunSuite
+import wacc.ast.*
+import wacc.q_ast.*
+
+class rename_func_test extends AnyFunSuite {
+
+
+}
