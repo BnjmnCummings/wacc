19c0bb130f2c25437ec960557d3fc3c4eb49655f
Jack Fletcher <jf623@ic.ac.uk>
2025-02-05 22:53:46 +0000

    fix - removed useless ErasedPairType + made TypeInfo accessible to main wacc folder

--- src/main/wacc/semType.scala
+++ src/main/wacc/semType.scala
@@ -1,4 +1,4 @@
-package wacc.semantic
+package wacc
 
 sealed abstract class SemType
 case object ? extends SemType
@@ -12,7 +12,15 @@ enum KnownType extends SemType {
     case String
     case Array(ty: SemType)
     case Pair(ty1: SemType, ty2: SemType)
-    case ErasedPairType
     case Ident
     // Note an erased pair can be created with ty1/ty2 = pair(?, ?)
 }
+
+// The typer will take this in
+// It maps variable names & function names to their types
+// This should allow for variables and functions to share names
+class TypeInfo(
+    var varTys: Map[String, KnownType],
+    var funcTys: Map[(String, KnownType), Map[String, KnownType]]
+    // This is a map from (Function Identifier, Return Type) -> parameter map [Param name -> Param type]
+)
