9d42e36d97b669cbfef3af0467963672d4d47bbc
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-02-05 19:59:09 +0000

    feat - wrote integration test to test that semantically valid programs in wacc-examples successfully scope/type check

---
+++ src/test/wacc/semantic/integration.test.scala
@@ -0,0 +1,91 @@
+package wacc.ast
+
+import wacc.parser
+import wacc.renamer
+import wacc.typeChecker
+import wacc.ScopeException
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
+
+import parsley.{Success, Failure}
+
+import collection.mutable.ListBuffer
+
+import java.io.File
+
+class semantic_integration_test extends AnyFlatSpec {
+    val validPaths: List[String] = getValidPaths()
+
+    val invalidPaths: List[String] = getInvalidPaths()
+
+    "parser" should "successfully parse and scope/type check valid wacc programs" in {
+        val synFailures: ListBuffer[String] = new ListBuffer()
+        val semFailures: ListBuffer[String] = new ListBuffer()
+        val successes: ListBuffer[String] = new ListBuffer()
+        validPaths.foreach {
+            p => parser.parseF(File(p)) match 
+                case Success(t) => {
+                    try {
+                        val q_t = renamer.rename(t)
+                        typeChecker.check(q_t)
+                        successes += p
+                    } catch {
+                        case e: ScopeException => {
+                            // type/scope checking has failed
+                            semFailures += p
+                        }
+                    }
+                }
+                case _ => synFailures.addOne(p)
+        }
+        val synFailList: List[String] = synFailures.toList
+        val semFailList: List[String] = semFailures.toList
+        val successList: List[String] = successes.toList
+        info("correctly succeeding tests:\n")
+        successList.map(s => s.split("valid/").last).foreach(info(_))
+        if (synFailList.length != 0) {
+            fail(
+                "some of the paths failed to parse (they are syntactically valid and should succeed):\n\n" 
+                + synFailList.foldRight("")((s1, s2) => s1 + "\n" + s2)
+            )
+        }
+        if (semFailList.length != 0) {
+            fail(
+                "some of the paths failed to scope/type check (they are semantically valid and should succeed):\n\n" 
+                + semFailList.foldRight("")((s1, s2) => s1 + "\n" + s2)
+            )
+        }
+    }
+
+    it should "reject invalid wacc programs" in {
+    }
+
+    def getValidPaths(): List[String] = {
+        val fPathStart: String = "wacc-examples/valid/"
+        searchFiles(File(fPathStart))
+    }
+
+    def getInvalidPaths(): List[String] = {
+        val fPathStart: String = "wacc-examples/invalid/semanticErr"
+        searchFiles(File(fPathStart))
+    }
+
+    // Function to recursively search for files in the given directory
+    // Chatgpt wrote this one
+    def searchFiles(dir: File): List[String] = {
+        if (dir.exists && dir.isDirectory) {
+            // List to collect all file paths
+            val filePaths = dir.listFiles.filter(_.isFile).map(_.getAbsolutePath).toList
+
+            // Recursively search in subdirectories
+            val subDirFiles = dir.listFiles.filter(_.isDirectory).flatMap(searchFiles).toList
+
+            // Combine files from current directory and subdirectories
+            filePaths ++ subDirFiles
+        } else {
+            List()  // Return empty list if the path is not a valid directory
+        }
+    }
+
+}
