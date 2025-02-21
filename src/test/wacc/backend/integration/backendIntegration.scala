89121788ce2f2906e86d13bbcd189956f23ddab4
bc2423 <bc2423@ic.ac.uk>
2025-02-21 21:26:22 +0000

    feat - added integration test class and rehoused shell files

--- src/test/wacc/backend/integration/backendIntegration.scala
+++ src/test/wacc/backend/integration/backendIntegration.scala
@@ -3,72 +3,70 @@ package wacc
 import sys.process._
 import java.io.FileNotFoundException
 import scala.io.Source
-//import wacc.utilities.searchDir
+import wacc.utilities.searchDir
 import java.io.File
-//import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.flatspec.AnyFlatSpec
 import scala.collection.mutable.ListBuffer
 
-// class backend_integration_test extends AnyFlatSpec {
+class backend_integration_test extends AnyFlatSpec {
     
-//     "backend" should "successfully produce the desired output and exit code" in {
+    "backend" should "successfully produce the desired output and exit code" in {
+        runAssembly("andExpr")
+        val failures: ListBuffer[String] = ListBuffer.empty[String]
+        val successes: ListBuffer[String] = ListBuffer.empty[String]
 
-//         runAssembly("/homes/bc2423/Wacc/WACC_16/src/test/wacc/backend/andExpr.s")
-//         // val failures: ListBuffer[String] = ListBuffer.empty[String]
-//         // val successes: ListBuffer[String] = ListBuffer.empty[String]
+        getFilePaths().foreach { 
+            fileName =>
+                //TODO: turn into an assembly file and put in src/test/wacc/backend/integration/assembly
+                val progName = fileName
+                    .split("/")
+                    .last
+                    .replace(".wacc","")
+                
+                if(runAssembly(progName) == getExpectedOutput(fileName)) {
+                    successes += fileName
+                } else {
+                    failures += fileName
+                }
+        }
 
-//         // getFilePaths().foreach { 
-//         //     filename =>
-//         //         //turn into an assembly file
-//         //         if(runAssembly(filename) == getExpectedOutput(filename)) {
-//         //             successes += filename
-//         //         } else {
-//         //             failures += filename
-//         //         }
-//         // }
+        val failList: List[String] = failures.toList
+        val successList: List[String] = successes.toList
 
-//         // val failList: List[String] = failures.toList
-//         // val successList: List[String] = successes.toList
+        /* report successful test cases */
+        info("correctly succeeding tests:\n")
+        successList.map(s => s.split("valid/").last).foreach(info(_))
 
-//         // /* report successful test cases */
-//         // info("correctly succeeding tests:\n")
-//         // successList.map(s => s.split("valid/").last).foreach(info(_))
+        /* report failing test cases */
+        if (failList.length != 0) {
+            fail(
+                "some of the paths failed (they are valid and should succeed):\n\n" 
+                + failList.map(s => s.split("valid/").last).mkString("\n")
+            )
+        }
+    } 
 
-//         // /* report failing test cases */
-//         // if (failList.length != 0) {
-//         //     fail(
-//         //         "some of the paths failed (they are valid and should succeed):\n\n" 
-//         //         + failList.map(s => s.split("valid/").last).mkString("\n")
-//         //     )
-//         // }
-//     } 
-
-//     // TODO: seperate the sourcefile with the generated files: perhaps into a build folder
-// }
-
-/**
-  * 
-  * @param filename the path to the generated 'progName.s' assembly file
-  * @return a pair: (exit code, output)
-  */
-def runAssembly(filename: String): (Int, List[String]) = {
+    /**
+    * 
+    * @param progName the path to the generated 'progName.s' assembly file
+    * @return a pair: (exit code, output)
+    */
+    def runAssembly(progName: String): (Int, List[String]) = {
         /* TODO: refactor with regex*/
-        val progName = "assembly/" + filename
-            .split("/")
-            .last
-            .replace(".s","")
-
-        val buildExitStatus = s"./buildAss $progName" .!
+        val fileName = "assembly/" + progName
+        val buildExitStatus = s"./buildAss $fileName" .!
 
         if (buildExitStatus == 0) {
-            val cmd = s"./$progName"
+            val cmd = s"./src/test/wacc/backend/integration/$fileName"
             val exitStatus = cmd .!
             val output: ListBuffer[String] = ListBuffer.empty[String]
 
             if(exitStatus == 0) {
                 output ++= (cmd .!!).split('\n')
             }
-            //clean up after ourselves and return
-            s"./wipeAss $progName" .!
+
+            /* clean up after ourselves and return */
+            s"./wipeAss $fileName" .!
             return (exitStatus, output.toList)
 
         } else {
@@ -77,12 +75,12 @@ def runAssembly(filename: String): (Int, List[String]) = {
     }
 
     /**
-     * @param filename the path to the .wacc file
+     * @param fileName the path to the .wacc file
      * @return an expected pair: (exit code, output)
      */
-    def getExpectedOutput(filename: String): (Int, List[String]) =
+    def getExpectedOutput(fileName: String): (Int, List[String]) =
         try {
-            val lines = Source.fromFile(filename).getLines().toList
+            val lines = Source.fromFile(fileName).getLines().toList
             val output = lines
                 .dropWhile( _ != "# Output:").tail
                 .takeWhile(s => s != "# Program:" && s != "# Exit:")
@@ -102,25 +100,31 @@ def runAssembly(filename: String): (Int, List[String]) = {
             return (exitCode, output) 
         } catch {
             case e: FileNotFoundException => {
-                throw FileNotFoundException(s"File Not Found: $filename")
+                throw FileNotFoundException(s"File Not Found: $fileName")
             } 
         }
 
-    //@main
-    def temp(filename: String) = {
-        val progName = filename
-            .split("/")
-            .last
-            .replace(".wacc","")
-        val assName = s"src/test/wacc/backend/integration/assembly/$progName"
+    // def temp(fileName: String) = {
+    //     val progName = fileName
+    //         .split("/")
+    //         .last
+    //         .replace(".wacc","")
         
-        println(getExpectedOutput(filename))
-        println(runAssembly(assName))
+    //     println(getExpectedOutput(fileName))
+    //     println(runAssembly(progName))
 
-        //println(s"result:${ getExpectedOutput(filename) == runAssembly(progName)}")
-    }
-
-    // def getFilePaths(): List[String] = {
-    //     val fPathStart: String = "wacc-examples/valid/"
-    //     searchDir(File(fPathStart))
     // }
+    
+    /** 
+     * Helper function to collect all the filepaths to valid wacc programs
+     * TODO: change back to all wacc programs
+     */ 
+    def getFilePaths(): List[String] = {
+        // val fPathStart: String = "wacc-examples/valid/"
+        // searchDir(File(fPathStart))
+        return List("wacc-examples/valid/expressions/andExpr.wacc")
+    }
+}
+
+
+
