2e7befb8e7ddab801a068b925b084a36c5affce6
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-02-25 16:28:25 +0000

    ci (zl4323, jf623) - refactored testSettings to use a hashSet

--- src/test/wacc/testUtils/conditionalRun.scala
+++ src/test/wacc/testUtils/conditionalRun.scala
@@ -4,11 +4,11 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 trait ConditionalRun extends AnyFlatSpec{
     def runIfTrue(
-        testSettings: scala.collection.mutable.HashMap[String, Boolean],
+        testSettings: scala.collection.immutable.Set[String],
         test: String,
         testBody: () => Unit
     ): Unit = {
-        if (testSettings.getOrElse(test, false)) {
+        if (testSettings.contains(test)) {
             testBody()
         } else {
             it should s"pass $test" in {
