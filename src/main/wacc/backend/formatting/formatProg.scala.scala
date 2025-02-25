dc22fd3d9ff03ffe954ce5f4583416819c2e1c5c
bc2423 <bc2423@ic.ac.uk>
2025-02-25 13:26:29 +0000

    feat - implemented formatting for programs

---
+++ src/main/wacc/backend/formatting/formatProg.scala.scala
@@ -0,0 +1,33 @@
+package wacc.formatting
+
+import wacc.assemblyIR.*
+
+def formatProg(prog: A_Prog): String = 
+    val sb = StringBuilder()
+    sb ++= formatHeaders(prog.data)
+    sb ++= prog.funcs.flatMap(formatFunction(_))
+    sb.toString()
+
+def formatHeaders(data: List[A_StoredStr]): String = 
+    val sb = StringBuilder()
+    sb ++= ".intel_syntax noprefix\n"
+    sb ++= ".globl main\n"    
+    sb ++= ".section .rodata\n"
+    sb ++= data.flatMap(formatData(_))
+    sb ++= ".text\n"
+    sb.toString()
+
+def formatData(storedStr: A_StoredStr): String = 
+    val sb = StringBuilder()
+    sb ++= s"\t.int ${storedStr.str.length()}\n"
+    //TODO: verify that storedStr.lbl contains the number :)
+    sb ++= s"${storedStr.lbl.name}:\n"
+    sb ++= s"\t.asciz: \"${storedStr.str}\"\n"
+    sb.toString()
+
+def formatFunction(func: A_Func): String = 
+    val sb = StringBuilder()
+    /* any reason why label can't just be a string? */
+    sb ++= s"${func.lbl.name}:"
+    sb.toString()
+
