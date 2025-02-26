269683d678a0b45b22a87fa3ec41774a57e40545
Aidan <aidanmadge@gmail.com>
2025-02-26 13:17:17 +0000

    refactor - remove useless comments

--- src/main/wacc/backend/formatting/formatProg.scala
+++ src/main/wacc/backend/formatting/formatProg.scala
@@ -20,14 +20,12 @@ def formatHeaders(data: List[A_StoredStr]): String =
 def formatData(storedStr: A_StoredStr): String = 
     val sb = StringBuilder()
     sb ++= s"\t.int ${storedStr.str.length()}\n"
-    //TODO: verify that storedStr.lbl contains the number :)
     sb ++= s"${storedStr.lbl.name}:\n"
     sb ++= s"\t.asciz \"${storedStr.str}\"\n"
     sb.toString()
 
 def formatFunction(func: A_Func): String = 
     val sb = StringBuilder()
-    /* any reason why label can't just be a string? */
     sb ++= s"${func.lbl.name}:"
     sb.toString()
 
