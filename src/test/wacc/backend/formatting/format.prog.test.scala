0afc22ac1a80dbeb8bc8f84272bf37c9d8f733be
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-02-26 15:18:27 +0000

    refactor (zl4323, bc2423) - refactored formatProg.scala to use a file writer instead of a string builderand changed tests accordingly

--- src/test/wacc/backend/formatting/format.prog.test.scala
+++ src/test/wacc/backend/formatting/format.prog.test.scala
@@ -3,6 +3,8 @@ package wacc.formatting
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
 
+import java.io.StringWriter
+
 import wacc.testUtils.*
 
 import wacc.assemblyIR.*
@@ -11,17 +13,23 @@ import wacc.assemblyIR.A_OperandSize
 class format_prog_test extends ConditionalRun {
     runIfTrue(testSettings, "format_prog_test", () => {
         "formatProg" should "format a program with no data and no functions" in {
+            val writer = new StringWriter()
             val prog = A_Prog(List(), List())
-            formatProg(prog) shouldBe ".intel_syntax noprefix\n" +
+            formatProg(prog)(using writer)
+            val progData = writer.toString()
+            progData shouldBe ".intel_syntax noprefix\n" +
               ".globl main\n." +
               "section .rodata\n" +
               ".text\n"
         }
 
         it should "format a program with no data and one function" in {
+            val writer = new StringWriter()
             val func = A_Func(A_InstrLabel("main"), List())
             val prog = A_Prog(List(), List(func))
-            formatProg(prog) shouldBe ".intel_syntax noprefix\n" +
+            formatProg(prog)(using writer)
+            val progData = writer.toString()
+            progData shouldBe ".intel_syntax noprefix\n" +
               ".globl main\n" +
               ".section .rodata\n" +
               ".text\n" +
@@ -29,11 +37,14 @@ class format_prog_test extends ConditionalRun {
         }
 
         it should "format a program with data and functions" in {
+            val writer = new StringWriter()
             val data = List(A_StoredStr(A_DataLabel("str"), "hello world!"))
             val func = A_Func(A_InstrLabel("main"), List())
             val func2 = A_Func(A_InstrLabel("main2"), List())
             val prog = A_Prog(data, List(func, func2))
-            formatProg(prog) shouldBe ".intel_syntax noprefix\n" +
+            formatProg(prog)(using writer)
+            val progData = writer.toString()
+            progData shouldBe ".intel_syntax noprefix\n" +
               ".globl main\n" +
               ".section .rodata\n" +
               "\t.int 12\n" +
@@ -49,7 +60,9 @@ class format_prog_test extends ConditionalRun {
 class format_headers_test extends ConditionalRun {
     runIfTrue(testSettings, "format_headers_test", () => {
         "formatHeaders" should "format headers for a program with no data" in {
-            val headers = formatHeaders(List())
+            val writer = new StringWriter()
+            formatHeaders(List())(using writer)
+            val headers = writer.toString()
             headers shouldBe ".intel_syntax noprefix\n" +
               ".globl main\n." +
               "section .rodata\n" +
@@ -57,8 +70,10 @@ class format_headers_test extends ConditionalRun {
         }
 
         it should "format headers for a program with one stored string" in {
+            val writer = new StringWriter()
             val data = List(A_StoredStr(A_DataLabel("str"), "hello world!"))
-            val headers = formatHeaders(data)
+            formatHeaders(data)(using writer)
+            val headers = writer.toString()
             headers shouldBe ".intel_syntax noprefix\n" +
               ".globl main\n" +
               ".section .rodata\n" +
@@ -69,11 +84,13 @@ class format_headers_test extends ConditionalRun {
         }
 
         it should "format headers for a program with multiple stored strings" in {
+            val writer = new StringWriter()
             val data = List(
                 A_StoredStr(A_DataLabel("str1"), "hello world!"),
                 A_StoredStr(A_DataLabel("str2"), "hello!")
             )
-            val headers = formatHeaders(data)
+            formatHeaders(data)(using writer)
+            val headers = writer.toString()
             headers shouldBe ".intel_syntax noprefix\n" +
               ".globl main\n" +
               ".section .rodata\n" +
@@ -85,20 +102,25 @@ class format_headers_test extends ConditionalRun {
               "\t.asciz \"hello!\"\n" +
               ".text\n"
         }
-
     })
 }
 
 class format_data_test extends ConditionalRun {
     runIfTrue(testSettings, "format_data_test", () => {
         "formatData" should "format a stored string" in {
+            val writer = new StringWriter()
             val storedStr = A_StoredStr(A_DataLabel("str"), "hello world!")
-            formatData(storedStr) shouldBe "\t.int 12\nstr:\n\t.asciz \"hello world!\"\n"
+            formatData(storedStr)(using writer)
+            val data = writer.toString()
+            data shouldBe "\t.int 12\nstr:\n\t.asciz \"hello world!\"\n"
         }
 
         it should "format an empty string" in {
+            val writer = new StringWriter()
             val storedStr = A_StoredStr(A_DataLabel("str"), "")
-            formatData(storedStr) shouldBe "\t.int 0\nstr:\n\t.asciz \"\"\n"
+            formatData(storedStr)(using writer)
+            val data = writer.toString()
+            data shouldBe "\t.int 0\nstr:\n\t.asciz \"\"\n"
         }
     })
 }
@@ -106,21 +128,30 @@ class format_data_test extends ConditionalRun {
 class format_function_test extends ConditionalRun {
     runIfTrue(testSettings, "format_function_test", () => {
         "formatFunction" should "format an empty function" in {
+            val writer = new StringWriter()
             val func = A_Func(A_InstrLabel("main"), List())
-            formatFunction(func) shouldBe "main:\n"
+            formatFunction(func)(using writer)
+            val funcData = writer.toString()
+            funcData shouldBe "main:\n"
         }
 
         it should "format a function with one instruction" in {
+            val writer = new StringWriter()
             val func = A_Func(A_InstrLabel("main"), List(A_Ret))
-            formatFunction(func) shouldBe "main:\n\tret\n"
+            formatFunction(func)(using writer)
+            val funcData = writer.toString()
+            funcData shouldBe "main:\n\tret\n"
         }
 
         it should "format a function with multiple instructions" in {
+            val writer = new StringWriter()
             val func = A_Func(
                 A_InstrLabel("main"), 
                 List(A_Add(A_Reg(A_OperandSize.A_64, A_RegName.RetReg), A_Imm(3), A_OperandSize.A_64), A_Ret)
             )
-            formatFunction(func) shouldBe "main:\n\tadd rax, 3\n\tret\n"
+            formatFunction(func)(using writer)
+            val funcData = writer.toString()
+            funcData shouldBe "main:\n\tadd rax, 3\n\tret\n"
         }
     })
 }
