df297f7633aa21f43f5ef7e6488d0f2bc9c57947
zl4323 <zakk.lazar23@imperial.ac.uk>
2025-02-26 16:30:31 +0000

    fix (zl4323, bc2423) - changed formatFunction to indent label names properly

--- src/main/wacc/backend/formatting/formatProg.scala
+++ src/main/wacc/backend/formatting/formatProg.scala
@@ -3,26 +3,26 @@ package wacc.formatting
 import wacc.assemblyIR.*
 import java.io.Writer
 
-def formatProg(prog: A_Prog)(using writer: Writer): Unit = 
+def formatProg(prog: A_Prog)(using writer: Writer) = 
     formatHeaders(prog.data)
     prog.funcs.foreach(formatFunction(_))
 
-def formatHeaders(data: List[A_StoredStr])(using writer: Writer): Unit = 
+def formatHeaders(data: List[A_StoredStr])(using writer: Writer) = 
     writer.write(".intel_syntax noprefix\n")
     writer.write(".globl main\n")
     writer.write(".section .rodata\n")
     data.foreach(formatData(_))
     writer.write(".text\n")
 
-def formatData(storedStr: A_StoredStr)(using writer: Writer): Unit = 
+def formatData(storedStr: A_StoredStr)(using writer: Writer) = 
     writer.write(s"\t.int ${storedStr.str.length()}\n")
     writer.write(s"${storedStr.lbl.name}:\n")
     writer.write(s"\t.asciz \"${storedStr.str}\"\n")
 
-def formatFunction(procedure: A_Proc)(using writer: Writer): Unit = procedure match
+def formatFunction(procedure: A_Proc)(using writer: Writer) = procedure match
     case A_Func(fLabel, instrs) => 
         writer.write(s"${fLabel.name}:\n")
-        instrs.foreach(instr => writer.write("\t" + formatInstr(instr) + "\n"))
+        formatBodyInstr(instrs)
 
     case A_DataFunc(fLabel, instrs, data) =>
         writer.write(".section .rodata\n")
@@ -31,4 +31,12 @@ def formatFunction(procedure: A_Proc)(using writer: Writer): Unit = procedure ma
         writer.write(s"\t.asciz \"${data.str}\"\n")
         writer.write(".text\n")
         writer.write(s"${fLabel.name}:\n")
-        instrs.foreach(instr => writer.write("\t" + formatInstr(instr) + "\n"))
+        formatBodyInstr(instrs)
+
+def formatBodyInstr(instrs: List[A_Instr])(using writer: Writer) =
+    instrs.foreach(instr => instr match 
+        case A_LabelStart(label) => writer.write(s"${label.name}:\n")
+        case _ => writer.write("\t" + formatInstr(instr) + "\n")
+    )
+    
+
