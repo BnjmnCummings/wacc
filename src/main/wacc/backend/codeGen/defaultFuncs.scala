6ef786ccb85b8b942183bafa9e10f215474d0fe1
Jack Fletcher <jf623@ic.ac.uk>
2025-02-27 15:45:09 +0000

    feat (jf623, zl4323) - implemented println default function

--- src/main/wacc/backend/codeGen/defaultFuncs.scala
+++ src/main/wacc/backend/codeGen/defaultFuncs.scala
@@ -5,7 +5,8 @@ import scala.collection.mutable.ListBuffer
 
 // stack aligning for 16 bytes
 inline def STACK_ALIGN_VAL = -16
-val OVERFLOW_LBL_STR_NAME = ".L._errOverflow_str0"
+val OVERFLOW_LBL_STR_NAME = ".L._errOverflow_str"
+val PRINTLN_LBL_STR_NAME = ".L._println_str"
 val ERR_EXIT_CODE = -1
 
 /* 
@@ -42,4 +43,35 @@ inline def defaultOverflow: A_Func = {
     program += A_Call(A_ExternalLabel("exit"))
 
     A_Func(A_InstrLabel("_errOverflow"), program.toList)
+}
+
+/*
+push rbp
+mov rbp, rsp
+# external calls must be stack-aligned to 16 bytes, accomplished by masking with fffffffffffffff0
+and rsp, -16
+lea rdi, [rip + .L._println_str0]
+call puts@plt
+mov rdi, 0
+call fflush@plt
+mov rsp, rbp
+pop rbp
+ret
+*/
+
+inline def defaultPrintln: A_Func = {
+    val program: ListBuffer[A_Instr] = ListBuffer()
+
+    program += A_Push(A_Reg(PTR_SIZE, A_RegName.BasePtr))
+    program += A_MovTo(A_Reg(PTR_SIZE, A_RegName.BasePtr), A_Reg(PTR_SIZE, A_RegName.StackPtr))
+    program += A_And(A_Reg(PTR_SIZE, A_RegName.StackPtr), A_Imm(STACK_ALIGN_VAL), PTR_SIZE)
+    program += A_Lea(A_Reg(PTR_SIZE, A_RegName.R1), A_MemOffset(PTR_SIZE, A_Reg(PTR_SIZE, A_RegName.InstrPtr), A_OffsetLbl(A_DataLabel(""))))
+    program += A_Call(A_ExternalLabel("puts"))
+    program += A_MovTo(A_Reg(PTR_SIZE, A_RegName.R1), A_Imm(ZERO_IMM))
+    program += A_Call(A_ExternalLabel("fflush"))
+    program += A_MovTo(A_Reg(PTR_SIZE, A_RegName.StackPtr), A_Reg(PTR_SIZE, A_RegName.BasePtr))
+    program += A_Pop(A_Reg(PTR_SIZE, A_RegName.BasePtr))
+    program += A_Ret
+
+    A_Func(A_InstrLabel("_println"), program.toList)
 }
