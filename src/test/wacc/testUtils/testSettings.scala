7c3e8cb148b47aafb6da8e5a5d3d5bff9b7d305d
Aidan <aidanmadge@gmail.com>
2025-03-01 01:50:06 +0000

    ci - add new bad char runtime testing to expected passing test suites

--- src/test/wacc/testUtils/testSettings.scala
+++ src/test/wacc/testUtils/testSettings.scala
@@ -54,7 +54,7 @@ val testSettings = scala.collection.immutable.HashSet(
     // "integration_IO_read",
     // "integration_pairs",
     // "integration_runtimeErr_arrayOutOfBounds",
-    // "integration_runtimeErr_badChar",
+    "integration_runtimeErr_badChar",
     // "integration_runtimeErr_divideByZero",
     // "integration_runtimeErr_integerOverflow",
     // "integration_runtimeErr_nullDereference",
