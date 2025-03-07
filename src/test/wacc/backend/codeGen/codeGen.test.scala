a5398387ffa6445a9b834852906b002131441602
BnjmnCummings <cummingsben097@gmail.com>
2025-03-07 15:59:03 +0000

    refactor - fixed tests to work with refactor

--- src/test/wacc/backend/codeGen/codeGen.test.scala
+++ src/test/wacc/backend/codeGen/codeGen.test.scala
@@ -1,113 +1,119 @@
 package wacc.codeGen
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.*
+import wacc.testUtils.*
 import wacc.t_ast.*
 import wacc.q_ast.Name
 import wacc.assemblyIR.*
 import wacc.TypeInfo
 import wacc.KnownType
 
-class code_gen_test extends AnyFlatSpec {
-    "genIntLiteral" should "generate correct assembly for integer literals" in {
-        given ctx: CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(), 
-                Map()
-            ), 
-            TableCtx(
-                StackTables(None)
-            )
-        )
-        val result = genIntLiteral(42)
-        result shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm(42), INT_SIZE))
-    }
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
 
-    "genBoolLiteral" should "generate correct assembly for boolean literals" in {
-        given ctx: CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(), 
-                Map()
-            ), 
-            TableCtx(
-                StackTables(None)
-            )
-        )
-        val resultTrue = genBoolLiteral(true)
-        val resultFalse = genBoolLiteral(false)
-        resultTrue shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm(TRUE), BOOL_SIZE))
-        resultFalse shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm(FALSE), BOOL_SIZE))
-    }
+class code_gen_test extends AnyFlatSpec, ConditionalRun {
 
-    "genCharLiteral" should "generate correct assembly for char literals" in {
-        given ctx: CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(), 
-                Map()
-            ), 
-            TableCtx(
-                StackTables(None)
+    runIfTrue(testSettings, "code_gen_test", () => {
+         "genIntLiteral" should "generate correct assembly for integer literals" in {
+            given ctx: CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(), 
+                    Map()
+                ), 
+                TableCtx(
+                    StackTables(None)
+                )
             )
-        )
-        val result = genCharLiteral('a')
-        result shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm('a'.toInt), CHAR_SIZE))
-    }
+            val result = genIntLiteral(42)
+            result shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm(42), INT_SIZE))
+        }
 
-    "genStringLiteral" should "generate correct assembly for string literals" in {
-        given ctx: CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(), 
-                Map()
-            ), 
-            TableCtx(
-                StackTables(None)
+        "genBoolLiteral" should "generate correct assembly for boolean literals" in {
+            given ctx: CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(), 
+                    Map()
+                ), 
+                TableCtx(
+                    StackTables(None)
+                )
             )
-        )
-        val result = genStringLiteral("hello")
-        result.nonEmpty shouldBe true
-    }
+            val resultTrue = genBoolLiteral(true)
+            val resultFalse = genBoolLiteral(false)
+            resultTrue shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm(TRUE), BOOL_SIZE))
+            resultFalse shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm(FALSE), BOOL_SIZE))
+        }
 
-    "genIdent" should "generate correct assembly for identifiers" in {
-        given ctx: CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(Name("x", 0) -> KnownType.Int), 
-                Map()
-            ), 
-            TableCtx(
-                StackTables(None)
+        "genCharLiteral" should "generate correct assembly for char literals" in {
+            given ctx: CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(), 
+                    Map()
+                ), 
+                TableCtx(
+                    StackTables(None)
+                )
             )
-        )
+            val result = genCharLiteral('a')
+            result shouldBe List(A_MovTo(A_Reg(A_RegName.RetReg), A_Imm('a'.toInt), CHAR_SIZE))
+        }
 
-        val stackTable = StackTable(0)
-        stackTable.addScope(
-            Set(Name("x", 0)), 
-            ctx.typeInfo
-        )
-        val stackTables = StackTables(Some(stackTable))
-        val result = genIdent(Name("x", 0), stackTables)
-        result shouldBe List(
-            A_MovTo(
-                A_Reg(A_RegName.RetReg), 
-                A_RegDeref(A_MemOffset(A_Reg(A_RegName.BasePtr), A_OffsetImm(16))), 
-                INT_SIZE
+        "genStringLiteral" should "generate correct assembly for string literals" in {
+            given ctx: CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(), 
+                    Map()
+                ), 
+                TableCtx(
+                    StackTables(None)
+                )
             )
-        )
-    }
+            val result = genStringLiteral("hello")
+            result.nonEmpty shouldBe true
+        }
 
-    "genAddSub" should "generate correct assembly for addition and subtraction" in {
-        given ctx: CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(), 
-                Map()
-            ), 
-            TableCtx(
-                StackTables(None)
+        "genIdent" should "generate correct assembly for identifiers" in {
+            given ctx: CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(Name("x", 0) -> KnownType.Int), 
+                    Map()
+                ), 
+                TableCtx(
+                    StackTables(None)
+                )
             )
-        )
-        val stackTable = StackTables(None)
-        val resultAdd = genAddSub(T_IntLiteral(1), T_IntLiteral(2), A_Add.apply, stackTable)
-        val resultSub = genAddSub(T_IntLiteral(3), T_IntLiteral(1), A_Sub.apply, stackTable)
-        resultAdd.nonEmpty shouldBe true
-        resultSub.nonEmpty shouldBe true
-    }
+
+            val stackTable = StackTable(0)
+            stackTable.addScope(
+                Set(Name("x", 0)), 
+                ctx.typeInfo
+            )
+            val stackTables = StackTables(Some(stackTable))
+            val result = genIdent(Name("x", 0), stackTables)
+            result shouldBe List(
+                A_MovTo(
+                    A_Reg(A_RegName.RetReg), 
+                    A_RegDeref(A_MemOffset(A_Reg(A_RegName.BasePtr), A_OffsetImm(16))), 
+                    INT_SIZE
+                )
+            )
+        }
+
+        "genAddSub" should "generate correct assembly for addition and subtraction" in {
+            given ctx: CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(), 
+                    Map()
+                ), 
+                TableCtx(
+                    StackTables(None)
+                )
+            )
+            val stackTable = StackTables(None)
+            val resultAdd = genAddSub(T_IntLiteral(1), T_IntLiteral(2), A_Add.apply, stackTable)
+            val resultSub = genAddSub(T_IntLiteral(3), T_IntLiteral(1), A_Sub.apply, stackTable)
+            resultAdd.nonEmpty shouldBe true
+            resultSub.nonEmpty shouldBe true
+        }
+    })
+   
 }
--- src/test/wacc/backend/codeGen/stackTable.test.scala
+++ src/test/wacc/backend/codeGen/stackTable.test.scala
@@ -1,165 +1,169 @@
 package wacc.codeGen
 
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers.*
+import wacc.testUtils.*
 import wacc.q_ast.Name
 import wacc.TypeInfo
 import wacc.KnownType
 
-class stack_table_test extends AnyFlatSpec {
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.*
 
-    "StackTable" should "be ablt to add variables to the table via addScope" in {
-        val stackTable = StackTable(0)
-        val typeInfo = TypeInfo(
-            Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
-            Map()
-        )
-        stackTable.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
+class stack_table_test extends AnyFlatSpec, ConditionalRun {
 
-        stackTable.contains(Name("x", 0)) shouldBe (true)
-        stackTable.contains(Name("y", 0)) shouldBe (true)
-    }
-
-    it should "return correct offset for variables" in {
-        val stackTable = StackTable(0)
-        val typeInfo = TypeInfo(
-            Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
-            Map()
-        )
-        stackTable.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
-
-        stackTable(Name("x", 0)) shouldBe (4)
-        stackTable(Name("y", 0)) shouldBe (8)
-    }
-
-    it should "throw a runtime exception when quereying offsets for variables not in scope" in {
-        val stackTable = StackTable(0)
-        val typeInfo = TypeInfo(
-            Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
-            Map()
-        )
-        stackTable.addScope(Set(), typeInfo)
-
-        a [RuntimeException] should be thrownBy stackTable(Name("x", 0))
-    }
-
-    it should "offset all the variables by <basePtrOffset>" in {
-        val stackTable = StackTable(4)
-        val typeInfo = TypeInfo(
-            Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
-            Map()
-        )
-        stackTable.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
-
-        stackTable(Name("x", 0)) shouldBe (8)
-        stackTable(Name("y", 0)) shouldBe (12)
-    }
-
-    it should "be able to add shadowed variables to the table" in {
-        val stackTable = StackTable(0)
-        val typeInfo = TypeInfo(
-            Map(Name("y", 0) -> KnownType.Int, Name("y", 1) -> KnownType.Int),
-            Map()
-        )
-        stackTable.addScope(Set(Name("y", 0)), typeInfo)
-        stackTable.addScope(Set(Name("y", 1)), typeInfo)
-
-        stackTable(Name("y", 0)) shouldBe 4
-        stackTable(Name("y", 1)) shouldBe 8
-    }
-
-    "StackTables" should "be able to add scopes correctly" in {
-        val stackTables = StackTables(None)
-        val typeInfo = TypeInfo(
-            Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
-            Map()
-        )
-        stackTables.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
-
-        stackTables.scopeSize shouldBe (8)
-    }
-
-    it should "be able to get variable from current scope" in {
-        given CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(Name("x", 0) -> KnownType.Int), 
+    runIfTrue(testSettings, "stack_table_test", () => {
+        "StackTable" should "be ablt to add variables to the table via addScope" in {
+            val stackTable = StackTable(0)
+            val typeInfo = TypeInfo(
+                Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
                 Map()
-            ),
-            TableCtx(
-                StackTables(None)
             )
-        )
+            stackTable.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
 
-        val stackTables = StackTables(None)
-        stackTables.addScope(Set(Name("x", 0)), summon[CodeGenCtx].typeInfo)
+            stackTable.contains(Name("x", 0)) shouldBe (true)
+            stackTable.contains(Name("y", 0)) shouldBe (true)
+        }
 
-        val instrs = stackTables.get(Name("x", 0))
-        instrs.nonEmpty shouldBe (true)
-    }
-
-    it should "throw exception if variable not found" in {
-        given CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(Name("x", 0) -> KnownType.Int), 
+        it should "return correct offset for variables" in {
+            val stackTable = StackTable(0)
+            val typeInfo = TypeInfo(
+                Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
                 Map()
-            ),
-            TableCtx(
-                StackTables(None)
             )
-        )
+            stackTable.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
 
-        val stackTables = StackTables(None)
+            stackTable(Name("x", 0)) shouldBe (4)
+            stackTable(Name("y", 0)) shouldBe (8)
+        }
 
-        a [RuntimeException] shouldBe thrownBy { stackTables.get(Name("y", 0)) }
-    }
-
-    it should "be ablt to set variable in current scope" in {
-        given CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(Name("x", 0) -> KnownType.Int), 
+        it should "throw a runtime exception when quereying offsets for variables not in scope" in {
+            val stackTable = StackTable(0)
+            val typeInfo = TypeInfo(
+                Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
                 Map()
-            ),
-            TableCtx(
-                StackTables(None)
             )
-        )
-        val stackTables = StackTables(None)
-        stackTables.addScope(Set(Name("x", 0)), summon[CodeGenCtx].typeInfo)
+            stackTable.addScope(Set(), typeInfo)
 
-        val instrs = stackTables.set(Name("x", 0))
-        instrs.nonEmpty shouldBe (true)
-    }
+            a [RuntimeException] should be thrownBy stackTable(Name("x", 0))
+        }
 
-    it should "throw exception if setting variable not found" in {
-        given CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(Name("x", 0) -> KnownType.Int), 
+        it should "offset all the variables by <basePtrOffset>" in {
+            val stackTable = StackTable(4)
+            val typeInfo = TypeInfo(
+                Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
                 Map()
-            ),
-            TableCtx(
-                StackTables(None)
             )
-        )
-        val stackTables = StackTables(None)
+            stackTable.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
 
-        a [RuntimeException] shouldBe thrownBy { stackTables.set(Name("y", 0)) }
-    }
+            stackTable(Name("x", 0)) shouldBe (8)
+            stackTable(Name("y", 0)) shouldBe (12)
+        }
 
-    it should "be able to generate argument store instructions" in {
-        given CodeGenCtx = CodeGenCtx(
-            TypeInfo(
-                Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int), 
+        it should "be able to add shadowed variables to the table" in {
+            val stackTable = StackTable(0)
+            val typeInfo = TypeInfo(
+                Map(Name("y", 0) -> KnownType.Int, Name("y", 1) -> KnownType.Int),
                 Map()
-            ),
-            TableCtx(
-                StackTables(None)
             )
-        )
-        val paramTable = StackTable(0)
-        paramTable.addScope(Set(Name("x", 0), Name("y", 0)), summon[CodeGenCtx].typeInfo)
-        val stackTables = StackTables(Some(paramTable))
+            stackTable.addScope(Set(Name("y", 0)), typeInfo)
+            stackTable.addScope(Set(Name("y", 1)), typeInfo)
 
-        val instrs = stackTables.argStoreInstrs(List(Name("x", 0), Name("y", 0)))
-        instrs.size shouldBe (2)
-    }
+            stackTable(Name("y", 0)) shouldBe 4
+            stackTable(Name("y", 1)) shouldBe 8
+        }
+
+        "StackTables" should "be able to add scopes correctly" in {
+            val stackTables = StackTables(None)
+            val typeInfo = TypeInfo(
+                Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int),
+                Map()
+            )
+            stackTables.addScope(Set(Name("x", 0), Name("y", 0)), typeInfo)
+
+            stackTables.scopeSize shouldBe (8)
+        }
+
+        it should "be able to get variable from current scope" in {
+            given CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(Name("x", 0) -> KnownType.Int), 
+                    Map()
+                ),
+                TableCtx(
+                    StackTables(None)
+                )
+            )
+
+            val stackTables = StackTables(None)
+            stackTables.addScope(Set(Name("x", 0)), summon[CodeGenCtx].typeInfo)
+
+            val instrs = stackTables.get(Name("x", 0))
+            instrs.nonEmpty shouldBe (true)
+        }
+
+        it should "throw exception if variable not found" in {
+            given CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(Name("x", 0) -> KnownType.Int), 
+                    Map()
+                ),
+                TableCtx(
+                    StackTables(None)
+                )
+            )
+
+            val stackTables = StackTables(None)
+
+            a [RuntimeException] shouldBe thrownBy { stackTables.get(Name("y", 0)) }
+        }
+
+        it should "be ablt to set variable in current scope" in {
+            given CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(Name("x", 0) -> KnownType.Int), 
+                    Map()
+                ),
+                TableCtx(
+                    StackTables(None)
+                )
+            )
+            val stackTables = StackTables(None)
+            stackTables.addScope(Set(Name("x", 0)), summon[CodeGenCtx].typeInfo)
+
+            val instrs = stackTables.set(Name("x", 0))
+            instrs.nonEmpty shouldBe (true)
+        }
+
+        it should "throw exception if setting variable not found" in {
+            given CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(Name("x", 0) -> KnownType.Int), 
+                    Map()
+                ),
+                TableCtx(
+                    StackTables(None)
+                )
+            )
+            val stackTables = StackTables(None)
+
+            a [RuntimeException] shouldBe thrownBy { stackTables.set(Name("y", 0)) }
+        }
+
+        it should "be able to generate argument store instructions" in {
+            given CodeGenCtx = CodeGenCtx(
+                TypeInfo(
+                    Map(Name("x", 0) -> KnownType.Int, Name("y", 0) -> KnownType.Int), 
+                    Map()
+                ),
+                TableCtx(
+                    StackTables(None)
+                )
+            )
+            val paramTable = StackTable(0)
+            paramTable.addScope(Set(Name("x", 0), Name("y", 0)), summon[CodeGenCtx].typeInfo)
+            val stackTables = StackTables(Some(paramTable))
+
+            val instrs = stackTables.argStoreInstrs(List(Name("x", 0), Name("y", 0)))
+            instrs.size shouldBe (2)
+        }
+    })
 }
