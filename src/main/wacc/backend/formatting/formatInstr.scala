2151b59361c0ee205dffecaaab00d14dfaa1dbbd
BnjmnCummings <cummingsben097@gmail.com>
2025-03-08 18:43:36 +0000

    refactor - added javadocs to the formatter

--- src/main/wacc/backend/formatting/formatInstr.scala
+++ src/main/wacc/backend/formatting/formatInstr.scala
@@ -1,17 +1,22 @@
 package wacc.formatting
 
 import wacc.assemblyIR.*
+
 import java.io.Writer
 
+/**
+ * Function to format an instruction in the assembly IR to a string.
+ * The formatted string is written to the IO stream writer.
+ * @param instr the instruction to be formatted
+ * @param writer the IO stream writer which accumulates assembly lines
+ */
 def formatInstr(instr: A_Instr)(using writer: Writer): Unit = instr match
-    // case A_Add(opD, opS, opSize) => s"add ${formatReg(opD, opSize)}, ${formatOperand(opS, opSize)}"
     case A_Add(opD, opS, opSize) => formatBinaryOp(opD, opS, opSize, opSize, "add")
     case A_Sub(opD, opS, opSize) => formatBinaryOp(opD, opS, opSize, opSize, "sub")
     case A_Div(opD, opS, opSize) => formatBinaryOp(opD, opS, opSize, opSize, "div")
     case A_IMul(opD, opS, opSize) => formatBinaryOp(opD, opS, opSize, opSize, "imul")
     case A_IDiv(opD, opSize) => formatUnaryOp(opD, opSize, "idiv")
 
-    // case A_Cmp(op1, op2, opSize) => s"cmp ${formatReg(op1, opSize)}, ${formatOperand(op2, opSize)}"
     case A_Cmp(op1, op2, opSize) => formatBinaryOp(op1, op2, opSize, opSize, "cmp")
     case A_Jmp(label, cond) => writer.write(s"j${formatCond(cond)} ${label.name}")
     case A_Set(op, cond) => writer.write(s"set${formatCond(cond)} ${{formatOperand(op, BOOL_SIZE)}}")
@@ -31,16 +36,15 @@ def formatInstr(instr: A_Instr)(using writer: Writer): Unit = instr match
         case A_ExternalLabel(name) => writer.write(s"call $name@plt")
         case A_DataLabel(_) => throw new IllegalArgumentException("Cannot call a data label")
         case A_DefaultLabel(name) => writer.write(s"call $name")
-    case A_Ret => writer.write("ret")
 
+    case A_Ret => writer.write("ret")
     case A_CDQ => writer.write("cdq")
 
-inline def formatBinaryOp(opD: A_Operand, opS: A_Operand, opSizeD: A_OperandSize, opSizeS: A_OperandSize, op: String)(using writer: Writer): Unit = 
-    writer.write(s"$op ${formatOperand(opD, opSizeD)}, ${formatOperand(opS, opSizeS)}")
-
-inline def formatUnaryOp(op: A_Operand, opSize: A_OperandSize, opName: String)(using writer: Writer): Unit = 
-    writer.write(s"$opName ${formatOperand(op, opSize)}")
-
+/**
+ * Function to format a condition in the assembly IR into a string.
+ * @param cond the condition to be formatted
+ * @return the formatted string representing the condition
+ */    
 def formatCond(cond: A_Cond): String = cond match
     case A_Cond.Eq => "e"
     case A_Cond.NEq => "ne"
@@ -51,3 +55,19 @@ def formatCond(cond: A_Cond): String = cond match
     case A_Cond.Overflow => "o"
     case A_Cond.NOverflow => "no"
     case A_Cond.Uncond => "mp"
+
+/**
+ * Function to format binary/unary operations in the assembly IR to a string.
+ * The formatted string is written to the IO stream writer.
+ * @param opD the destination operand for the operation
+ * @param opS the source operand for the operation
+ * @param opSizeD the size of the destination operand
+ * @param opSizeS the size of the source operand
+ * @param op the operation to be performed
+ * @param writer the IO stream writer which accumulates assembly lines
+ */
+inline def formatBinaryOp(opD: A_Operand, opS: A_Operand, opSizeD: A_OperandSize, opSizeS: A_OperandSize, op: String)(using writer: Writer): Unit = 
+    writer.write(s"$op ${formatOperand(opD, opSizeD)}, ${formatOperand(opS, opSizeS)}")
+
+inline def formatUnaryOp(op: A_Operand, opSize: A_OperandSize, opName: String)(using writer: Writer): Unit = 
+    writer.write(s"$opName ${formatOperand(op, opSize)}")
--- src/main/wacc/backend/formatting/formatProg.scala
+++ src/main/wacc/backend/formatting/formatProg.scala
@@ -1,12 +1,23 @@
 package wacc.formatting
 
 import wacc.assemblyIR.*
+
 import java.io.Writer
 
+/**
+ * Formats the given program into assembly code and writes it to the provided writer.
+ * @param prog The program to format.
+ * @param writer The writer to write the formatted assembly code to.
+ */
 def formatProg(prog: A_Prog)(using writer: Writer) = 
     formatHeaders(prog.data)
     prog.funcs.foreach(formatFunction(_))
 
+/**
+ * Formats the headers of the given data section and writes it to the provided writer.
+ * @param data The list of stored strings to format.
+ * @param writer The writer to write the formatted headers to.
+ */
 def formatHeaders(data: List[A_StoredStr])(using writer: Writer) = 
     writer.write(".intel_syntax noprefix\n")
     writer.write(".globl main\n")
@@ -14,15 +25,25 @@ def formatHeaders(data: List[A_StoredStr])(using writer: Writer) =
     data.foreach(formatData(_))
     writer.write(".text\n")
 
+/**
+ * Formats the given stored string and writes it to the provided writer.
+ * @param storedStr The stored string to format.
+ * @param writer The writer to write the formatted stored string to.
+ */
 def formatData(storedStr: A_StoredStr)(using writer: Writer) = 
     writer.write(s"\t.int ${storedStr.str.length()}\n")
     writer.write(s"${storedStr.lbl.name}:\n")
     writer.write(s"\t.asciz \"${storedStr.str}\"\n")
 
+/**
+ * Formats the given function or data function and writes it to the provided writer.
+ * @param procedure The function or data function to format.
+ * @param writer The writer to write the formatted function to.
+ */
 def formatFunction(procedure: A_SubRoutine)(using writer: Writer) = procedure match
     case A_Func(fLabel, instrs) => 
         writer.write(s"${fLabel.name}:\n")
-        formatBodyInstr(instrs)
+        formatBody(instrs)
 
     case A_DataFunc(fLabel, instrs, data) =>
         writer.write(".section .rodata\n")
@@ -31,9 +52,14 @@ def formatFunction(procedure: A_SubRoutine)(using writer: Writer) = procedure ma
         writer.write(s"\t.asciz \"${data.str}\"\n")
         writer.write(".text\n")
         writer.write(s"${fLabel.name}:\n")
-        formatBodyInstr(instrs)
+        formatBody(instrs)
 
-def formatBodyInstr(instrs: List[A_Instr])(using writer: Writer) =
+/**
+ * Formats the given list of instructions and writes them to the provided writer.
+ * @param instrs The list of instructions to format.
+ * @param writer The writer to write the formatted instructions to.
+ */
+def formatBody(instrs: List[A_Instr])(using writer: Writer) =
     instrs.foreach(instr => instr match 
         case A_LabelStart(label) => writer.write(s"${label.name}:\n")
         case _ => 
@@ -41,5 +67,3 @@ def formatBodyInstr(instrs: List[A_Instr])(using writer: Writer) =
             formatInstr(instr)
             writer.write("\n")
     )
-    
-
